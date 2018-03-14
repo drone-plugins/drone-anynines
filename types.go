f4139d575cf5a4ba27a59c01134be697b7eb90af
Thomas Boerger <thomas@webhippie.de>
2018-03-14 23:14:40 +0100

    Replaced old build process with current drone config

--- types.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Params struct {
-	Username     string `json:"username"`
-	Password     string `json:"password"`
-	Organization string `json:"organization"`
-	Space        string `json:"space"`
-	SkipCleanup  bool   `json:"skip_cleanup"`
-}
